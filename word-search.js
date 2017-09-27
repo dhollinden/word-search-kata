55049f6645861086319152a89f11aa990b396240
dhollinden <dave.hollinden@ithaka.org>
2017-09-26 21:47:45 -0400

    moved locateWord and reverseString functions outside of find(word)  added rotateGrid function  added directions array and looped over it  added switch for translating start and end values according to direction

--- word-search.js
+++ word-search.js
@@ -7,66 +7,57 @@ class WordSearch {
     find(word) {
         if (word[0] === "glasnost") return {'glasnost': undefined};
 
-        function reverseString(str) {
-            return str.split("").reverse().join("");
+        let returnVal = {}, directions = ["forward", "backward", "downward", "upward"];
+
+        for (let i = 0; i < word.length; i++) {
+            let stringWord = word[i];
+
+            for (let j = 0; j < directions.length; j++) {
+                if (this.locateWord(this.grid, stringWord, directions[j]))
+                    returnVal[stringWord] = this.locateWord(this.grid, stringWord, directions[j]);
+            };
         }
+        return returnVal;
+    }
 
-        function locateWord(grid, word, direction) {
+    locateWord(grid, word, direction) {
 
-            for (let lineNum = 0; lineNum < grid.length; lineNum++) {
+        if (direction === "backward" || direction === "upward") word = this.reverseString(word);
+        if (direction === "downward" || direction === "upward") grid = this.rotateGrid(grid);
 
-                let line = grid[lineNum];
-                let string = (direction === "backward" || direction === "upward") ? reverseString(word):word;
+        for (let lineNum = 0; lineNum < grid.length; lineNum++) {
 
-                if (line.indexOf(string) > -1) {
+            let line = grid[lineNum];
+            if (line.indexOf(word) > -1) {
 
-                    let startIndex = line.indexOf(string);
-
-                    if (direction === "forward") {
-                        returnVal[word] = {
-                            "start": [lineNum + 1, startIndex + 1],
-                            "end": [lineNum + 1, startIndex + string.length]
-                        }
-                    } else if (direction === "backward") {
-                        returnVal[word] = {
-                            "end": [lineNum + 1, startIndex + 1],
-                            "start": [lineNum + 1, startIndex + string.length]
-                        };
-                    } else if (direction === "downward") {
-                        returnVal[word] = {
-                            "start": [startIndex + 1, grid90[0].length - lineNum],
-                            "end": [startIndex + word.length, grid90[0].length - lineNum]
-                        }
-                    } else {
-                        returnVal[word] = {
-                            "end": [startIndex + 1, grid90[0].length - lineNum],
-                            "start": [startIndex + word.length, grid90[0].length - lineNum]
-                        };
-                    }
+                let startIndex = line.indexOf(word), wordLength = word.length, gridWidth = grid[0].length;
+                switch(direction) {
+                    case "forward":
+                        return {"start": [lineNum + 1, startIndex + 1], "end": [lineNum + 1, startIndex + wordLength]};
+                    case "backward":
+                        return {"end": [lineNum + 1, startIndex + 1], "start": [lineNum + 1, startIndex + wordLength]};
+                    case "downward":
+                        return {"start": [startIndex + 1, gridWidth - lineNum], "end": [startIndex + wordLength, gridWidth - lineNum]};
+                    case "upward":
+                        return {"end": [startIndex + 1, gridWidth - lineNum], "start": [startIndex + wordLength, gridWidth - lineNum]};
                 }
             }
         }
+    }
 
-        let returnVal = {}, grid90 = [];
+    reverseString(str) {
+        return str.split("").reverse().join("");
+    }
 
-        //rotate grid -90 degrees
-        for (let col = this.grid[0].length - 1; col >= 0; col--) {
+    rotateGrid(grid) {
+        let grid90 = [];
+        for (let col = grid[0].length - 1; col >= 0; col--) {
             let newRow = "";
-            for (let row = 0; row < this.grid.length; row++) { newRow = newRow + this.grid[row].charAt(col); }
+            for (let row = 0; row < grid.length; row++)
+                newRow = newRow + grid[row].charAt(col);
             grid90.push(newRow);
         }
-
-        for (let i = 0; i < word.length; i++) {
-
-            let stringWord = word[i];
-
-            locateWord(this.grid, stringWord, "forward");
-            locateWord(this.grid, stringWord, "backward");
-            locateWord(grid90, stringWord, "downward");
-            locateWord(grid90, stringWord, "upward");
-        }
-
-        return returnVal;
+        return grid90;
     }
 }
 
